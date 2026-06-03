a3acbc103e4aec61e272bfb7af8b3721a2f92950
Professor Michel Santos <154631555+ProfessorMichelSantos@users.noreply.github.com>
2026-06-03 18:01:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -96,3 +96,4 @@ def upsert_produto(produto_id: int, produto: ProdutoSchema, response: Response):
 # Inicializador do Uvicorn para rodar o arquivo diretamente
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    
